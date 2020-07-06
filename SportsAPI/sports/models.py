9ae4b9fab60cdfe896ac1976847d7da54549f961
Aman <bhatnagar.aman1998@gmail.com>
2020-07-07 01:17:40 +0530

    Final code changes

---
+++ SportsAPI/sports/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class sports(models.Model):      # Creating model class
+    name = models.CharField(max_length=20) 
+    sport = models.CharField(max_length=10)
+    age = models.IntegerField()  
+    def __str__(self):  
+        return self.name
